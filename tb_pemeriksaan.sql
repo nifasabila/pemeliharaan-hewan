6848bbdf6217181155f4b4c9c506dec488575e75
nifasabila <109188525+nifasabila@users.noreply.github.com>
2022-11-09 13:47:56 +0700

    Add files via upload

---
+++ tb_pemeriksaan.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 09, 2022 at 07:47 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pemeriksaanhewan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pemeriksaan`
+--
+
+CREATE TABLE `tb_pemeriksaan` (
+  `id` int(10) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `jh` enum('kucing','anjing','ular') NOT NULL,
+  `keluhan` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_pemeriksaan`
+--
+
+INSERT INTO `tb_pemeriksaan` (`id`, `nama`, `jh`, `keluhan`) VALUES
+(3, 'bela', 'anjing', 'pns');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_pemeriksaan`
+--
+ALTER TABLE `tb_pemeriksaan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_pemeriksaan`
+--
+ALTER TABLE `tb_pemeriksaan`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
